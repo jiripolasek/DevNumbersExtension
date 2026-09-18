a2262ecb52dc94ec926c0b5dc84abe6e34287000
Jiří Polášek <me@jiripolasek.com>
2026-09-18 16:15:17 +0200

    Oh wait, was this important?

--- src/DevNumbers/Helpers/NumberQuery.cs
+++ src/DevNumbers/Helpers/NumberQuery.cs
@@ -27,6 +27,12 @@ internal sealed record NumberQuery(
 
         var bitLength = query.Options.BitLength;
         var value = number.Value;
+
+        // a) If a bit length is specified, apply it to the parsed input:
+        //    values that do not fit are truncated to that bit length.
+        // b) If no bit length is specified:
+        //    - for negative values, automatically determine the best bit length;
+        //    - for positive values, leave the bit length unspecified.
         if (bitLength > 0)
         {
             value = value.TrimToBitLength(bitLength);
